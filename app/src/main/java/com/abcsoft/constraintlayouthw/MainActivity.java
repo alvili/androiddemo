bc1102f86a73c0a3f46a7d92ed886a176a444b0b
alvili <alvila2019@gmail.com>
2019-06-07 14:14:07 +0200

    segundo commit

--- app/src/main/java/com/abcsoft/constraintlayouthw/MainActivity.java
+++ app/src/main/java/com/abcsoft/constraintlayouthw/MainActivity.java
@@ -9,13 +9,13 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    long goodbyeTime=2;
-    private TextView txtTime;
-    private Date fecha =new Date();
-
     static{
         Log.d("INFO","SE INICIALIZA EL 'MUNDO ESTATICO'");
     }
+
+    long goodbyeTime = System.currentTimeMillis();
+    private TextView txtTime;
+    private Date fecha =new Date();
 
     public MainActivity(){
         Log.d("INFO","DENTRO DEL CONSTRUCTOR");
@@ -27,32 +27,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtTime = (TextView) findViewById(R.id.idTextViewTime);
-
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d("INFO","DENTRO DE ONPAUSE");
+        fecha = new Date();
         goodbyeTime = System.currentTimeMillis();
-        Log.d("INFO","DENTRO DE ONPAUSE" + goodbyeTime);
-        //fecha = new Date().getTime();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("INFO","DENTRO DE ONRESUME" + goodbyeTime);
+        Log.d("INFO","DENTRO DE ONRESUME");
 
-//        long milisegundos = (new Date()).getTime() - fecha.getTime();
-//        StringBuilder sb = new StringBuilder();
-//        sb.append(milisegundos/1000).append(" segundos transcuridos");
+        long milisegundos = (new Date()).getTime() - fecha.getTime();
+        StringBuilder sb = new StringBuilder();
+        sb.append(milisegundos/1000).append(" segundos transcuridos");
+        txtTime.setText(sb.toString());
 
-        long onPauseTime;
-        onPauseTime = System.currentTimeMillis() - goodbyeTime;
-        Log.d("INFO", String.valueOf(onPauseTime) );
-
-        txtTime.setText(String.valueOf(onPauseTime));
-//        txtTime.setText(sb.toString());
+//        txtTime.setText(String.valueOf((System.currentTimeMillis() - goodbyeTime) / 1000));
 
     }
 }
